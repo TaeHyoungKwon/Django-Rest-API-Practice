e71e0435eb4dbc069e7397a0d047a418d2af41b3
Hoorooroob <kth5604@gmail.com>
2019-03-17 06:13:08 +0900

    A Json Response

--- src/updates/views.py
+++ src/updates/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+from .models import Update
+
+
+def update_model_detail_view(request):
+    '''
+    JsonResponse를 통해서, 사진 타입의 data 변수를 Json 객체로 변환하여
+    응답 값을 줄 수 있다.
+    '''
+
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return JsonResponse(data)
